476dd1e41be6e400233bf93685d730e9862cee67
Marjanno Evert <marjanno.evert@tktk.ee>
2024-08-28 21:30:11 +0300

    Week 15 files and OOP

---
+++ Week15OOPAndFiles/Program.cs
@@ -0,0 +1,45 @@
+﻿List<Movie> myMovies = [];
+string[] data = GetDataFromMyFile();
+//ReadDataFromArray(data);
+
+foreach (string line in data)
+{
+    string[] tempArray = line.Split([';'], StringSplitOptions.RemoveEmptyEntries);
+    Movie newMovie = new(tempArray[0], tempArray[2]);
+    myMovies.Add(newMovie);
+}
+
+foreach (Movie movie in myMovies)
+{
+    Console.WriteLine($"One of my favorite movies {movie.Title} was relased in {movie.Year}.");
+}
+
+static string[] GetDataFromMyFile()
+{
+    string filePath = @"C:\data\movies.txt";
+    return File.ReadAllLines(filePath);
+}
+
+static void ReadDataFromArray(string[] someArray)
+{
+    foreach (string line in someArray)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+class Movie(string _title, string _year)
+{
+    private readonly string title = _title;
+    private readonly string year = _year;
+
+    public string Title
+    {
+        get { return title; }
+    }
+
+    public string Year
+    {
+        get { return year; }
+    }
+}
